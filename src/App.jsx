e39e149734ed38bca6486f7f77d423949f404da1
Eugene Wekesa <eugenco578@gmail.com>
2025-04-23 15:47:46 +0300

    modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cart from "./components/cart/Cart";
 import Checkout from "./pages/checkout";
+import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
