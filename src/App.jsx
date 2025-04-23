bf7d1e39eba766681f31d977789e8e99e95dbf31
Eugene Wekesa <eugenco578@gmail.com>
2025-04-23 17:44:36 +0300

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cart from "./components/cart/Cart";
-import Checkout from "./pages/checkout";
-import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <div>
+      <Cart />
+    </div>
   );
 }
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,38 +1,50 @@
-import React, { useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import CartItem from "./CartItem";
 import "./Cart.css";
-import { CartContext } from "../../context/CartContext";
 
 export default function Cart() {
-  const { cartItems, initializeCart, increaseQuantity, decreaseQuantity } = useContext(CartContext);
-  const navigate = useNavigate();
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    if (cartItems.length === 0) {
-      fetch("http://localhost:3000/dishes")
-        .then((res) => res.json())
-        .then((data) => {
-          initializeCart(data);
-        });
-    }
-  }, [cartItems, initializeCart]);
+    fetch("http://localhost:3000/dishes")
+      .then((res) => res.json())
+      .then((data) => {
+        const updatedData = data.map((item) => ({
+          ...item,
+          quantity: 1,
+        }));
+        setCartItems(updatedData);
+      });
+  }, []);
 
-  const handleCheckout = () => {
-    navigate("/checkout");
+  const handleIncrease = (id) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
   };
-
+  const handleDecrease = (id) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
   return (
     <div>
       {cartItems.map((item) => (
         <CartItem
           key={item.id}
           item={item}
-          onIncrease={() => increaseQuantity(item.id)}
-          onDecrease={() => decreaseQuantity(item.id)}
+          onIncraese={() => handleIncrease(item.id)}
+          onDecrease={() => handleDecrease(item.id)}
         />
       ))}
-      <button onClick={handleCheckout}>Proceed to Checkout</button>
     </div>
   );
 }
